15226d3ba644ad09b8a885e6ae0e395952fd025c
lpromotion <lpromotion00@gmail.com>
2023-04-01 21:09:47 +0900

    springboot test

--- SpringBoot/src/main/java/com/dca/spring/Application.java
+++ SpringBoot/src/main/java/com/dca/spring/Application.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+
+		// 메모리 사용량 출력
+		long heapSize = Runtime.getRuntime().totalMemory();
+		System.out.println("HEAP Size(M) : "+ heapSize / (1024*1024) + " MB");
 	}
 
 }
--- SpringBoot/src/test/java/com/dca/spring/ApplicationTests.java
+++ SpringBoot/src/test/java/com/dca/spring/ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.dca.spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
