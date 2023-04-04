f040e549d6b543291c345aeb1ed1230b6dc481d8
hogbal <hogbal31@gmail.com>
2023-04-04 16:35:04 +0900

    ADD blacklist.sh

--- Script/blacklist.sh
+++ Script/blacklist.sh
@@ -9,7 +9,11 @@ else
     ip_list=$1
     
     while read ip; do
-        # echo $ip
-        iptables -A INPUT -s $ip -j DROP
+        if [[ $ip =~ ^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$ ]]
+        then
+            iptables -A INPUT -s $ip -j DROP
+        else
+            echo "Error $ip"
+        fi
     done < $ip_list
 fi
