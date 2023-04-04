2877e97231b449094907b5d17e9da70b67fd29f4
hogbal <hogbal31@gmail.com>
2023-04-04 15:52:15 +0900

    ADD blacklist.sh

--- Script/blacklist.sh
+++ Script/blacklist.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
 
+echo "start blacklist.sh"
+
+if [ $# -lt 1 ]
+then
+    echo "sh blacklist.sh <filename>"
+else
+    ip_list=$1
+    
+    while read ip; do
+        # echo $ip
+        iptables -A INPUT -s $ip -j DROP
+    done < $ip_list
+fi
